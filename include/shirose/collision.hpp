be36a2fdbba2dd857a8250ba1c391659a4005936
Sho Hirose <sho.hirose@gmail.com>
2020-08-16 21:47:48 +0900

    style: Add comments

--- include/shirose/collision.hpp
+++ include/shirose/collision.hpp
@@ -30,8 +30,15 @@ bool doCollide(const Eigen::Vector2d& x1, const Eigen::Vector2d& x2,
 ///
 /// If two particles will not collide, the maximum value of double is returned.
 double calcCollisionTime(const Eigen::Vector2d& x1, const Eigen::Vector2d& x2,
-                     const Eigen::Vector2d& v1, const Eigen::Vector2d& v2,
-                     double r1, double r2);
+                         const Eigen::Vector2d& v1, const Eigen::Vector2d& v2,
+                         double r1, double r2);
+
+/// @brief Computes collision times between particles.
+/// @param[in] particles Particles
+///
+/// If a particle will not collide, its collision time is set to the maximum
+/// value of double.
+std::vector<double> calcCollisionTime(const ParticleSystem& particles);
 
 /// @brief Comutes velocities after collision.
 /// @param[in] v1 Velocity of particle 1
@@ -42,8 +49,6 @@ double calcCollisionTime(const Eigen::Vector2d& x1, const Eigen::Vector2d& x2,
 std::pair<NotAlignedVector2d, NotAlignedVector2d> calcVelocitiesAfterCollision(
     const Eigen::Vector2d& v1, const Eigen::Vector2d& v2, double m1, double m2,
     double e);
-
-std::vector<double> calcCollisionTime(const ParticleSystem& particles);
 
 }  // namespace shirose
 
