2c1b6d89e9098683b9e59c94b6e50a2ee1bec6b8
Sho Hirose <sho.hirose@gmail.com>
2020-08-22 13:05:46 +0900

    refactor: Remove previous positions and velocities

--- include/shirose/particle_system.hpp
+++ include/shirose/particle_system.hpp
@@ -9,25 +9,16 @@
 namespace shirose {
 
 struct ParticleSystem {
+  /// Use Eigen::aligned allocator for Eigen::Vector2d.
+  /// This is because Eigen::Vector2d requires aligned memory allocator.
+  /// Please refer to:
+  /// https://eigen.tuxfamily.org/dox/group__TopicStlContainers.html
   template <typename T>
   using StdVector = std::vector<T, Eigen::aligned_allocator<T>>;
 
-  /// @brief Positions of particles at the current time step
   StdVector<Eigen::Vector2d> positions;
-
-  /// @brief Velocities of particles at the current time step
   StdVector<Eigen::Vector2d> velocities;
-
-  /// @brief Positions of particles at the previous time step
-  StdVector<Eigen::Vector2d> previousPositions;
-
-  /// @brief Velocities of particles at the previous time step
-  StdVector<Eigen::Vector2d> previousVelocities;
-
-  /// @brief Masses of particles
   std::vector<double> masses;
-
-  /// @brief Radii of particles
   std::vector<double> radii;
 
    /// @brief Returns the number of particles.
@@ -36,12 +27,6 @@ struct ParticleSystem {
            this->positions.size() == this->masses.size() &&
            this->positions.size() == this->radii.size());
     return this->positions.size();
-  }
-
-  /// @brief Stores the current velocities and positions in the previous ones.
-  void storePreviousTimeStep() noexcept {
-    this->previousPositions = this->positions;
-    this->previousVelocities = this->velocities;
   }
 };
 
